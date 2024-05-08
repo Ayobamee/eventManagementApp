76936fd443a05ed8df0e8a1f421cba1d795ad878
Ayobami Elutade <aelutade@1wa.org>
2024-05-08 16:23:30 +0100

    added tests for ticket details

--- cypress/pages/EventPage.js
+++ cypress/pages/EventPage.js
@@ -1,5 +1,8 @@
-import { faker } from '@faker-js/faker'
+import { faker, th } from '@faker-js/faker'
 const eventName = `Event-${faker.company.catchPhrase()}`
+const venueAddress = `Venue-${faker.company.catchPhrase()}`
+const description = `Description-${faker.company.catchPhrase()}`
+const ticketName = `TicketName-${faker.company.catchPhrase()}`
 
 class EventPage {
   elements = {
@@ -15,6 +18,25 @@ class EventPage {
     ticketDetailsDiv: () => cy.get('li').contains('Ticket Details'),
     eventControlsDiv: () => cy.get('li').contains('Event Controls'),
     eventNameInputField: () => cy.get('input[name="event_name"]'),
+    eventDateInputField: () => cy.get('input[name="event_date"]'),
+    eventTimeInputField: () => cy.get('input[name="event_time"]'),
+    specific2025calendar: () => cy.get('div.calendar-table .yearselect'),
+    specificWeekend: () => cy.get('td.weekend.available'),
+    applyBtn: () => cy.get('button').contains('Apply'),
+    eighthApplyBtn: () => cy.get('button[type="button"]'),
+    venueaddressInputField: () => cy.get('input[placeholder="Venue address"]'),
+    descriptionnameInputField: () => cy.get('textarea[name="event_desc"]'),
+    privateEventTypeBtn: () => cy.get('input[value="PRIVATE"]'),
+    freePaymentTypeBtn: () => cy.get('input[value="FREE"]'),
+    nextBtn: () => cy.get('button').contains('Next'),
+    ticketDetailsText: () => cy.get('div').contains('Ticket Details'),
+    addTicketBtn: () => cy.get('a').contains('Add ticket'),
+    ticketNameInputField: () => cy.get('input[name="ticket_name"]'),
+    noOfTickets: () => cy.get('input[name="ticket_population"]'),
+    ticketDescriptionInputField: () =>
+      cy.get('input[placeholder="Ticket description"]'),
+    nextButton: () => cy.get('button'),
+    eventControlsText: () => cy.get('div').contains('Event Controls'),
   }
 
   checkEventPageItems() {
@@ -35,6 +57,24 @@ class EventPage {
     this.elements.ticketDetailsDiv().should('be.visible')
     this.elements.eventControlsDiv().should('be.visible')
     this.elements.eventNameInputField().type(eventName)
+    this.elements.eventDateInputField().click()
+    this.elements.specific2025calendar().first().select('2025')
+    this.elements.specificWeekend().first().click()
+    this.elements.applyBtn().click()
+    this.elements.eventTimeInputField().click()
+    this.elements.eighthApplyBtn().eq(7).click({ force: true })
+    this.elements.venueaddressInputField().type(venueAddress)
+    this.elements.descriptionnameInputField().type(description)
+    this.elements.privateEventTypeBtn().click()
+    this.elements.freePaymentTypeBtn().click()
+    this.elements.nextBtn().click()
+    this.elements.ticketDetailsText().should('be.visible')
+    this.elements.addTicketBtn().click()
+    this.elements.ticketNameInputField().type(ticketName)
+    this.elements.noOfTickets().type('5')
+    this.elements.ticketDescriptionInputField().type('10')
+    this.elements.nextButton().eq(2).click({ force: true })
+    this.elements.eventControlsText().should('be.visible')
   }
 }
 
